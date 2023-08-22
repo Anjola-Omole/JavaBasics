5fc4e466de917d9a33ef2ca7eae9e6a370cfdb0a
Anjola Omole <anjola567@gmail.com>
2023-08-22 18:07:52 -0500

    Second Task Done

---
+++ src/secondTask.java
@@ -0,0 +1,15 @@
+public class secondTask {
+
+    public static void main(String[] args) {
+        int [] num = {42,4,535,342,56,434};
+        int sum = 0;
+        for(int i = 0; i< num.length; i++){
+            sum = sum + num[i];
+
+        }
+        System.out.println(sum);
+    }
+
+
+
+}
